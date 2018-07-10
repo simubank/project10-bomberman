14fd4dfa2b0c0c65e2a814fa488f45927eae7f05
CannedCans <CannedCans@users.noreply.github.com>
2018-07-10 14:15:58 -0400

    Initial router code

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,14 @@
 //
+
+// The auth key for the LevelUp Quest API
+var auth_key = '';
+const PORT = 3000; // The port the server will listen on
+//
+
+var express = require('express');
+var app = express();
+
+var routes = require('./router');
+app.use('/', routes);
+
+app.listen(PORT);
---
+++ backend/routes.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+const request = require('request');
+
+
+module.exports = router;
