80a8efb0da42904e57a94456dafc49a5d9ab41d9
CannedCans <CannedCans@users.noreply.github.com>
2018-07-10 14:18:43 -0400

    Added auth key

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,7 @@
 //
 
 // The auth key for the LevelUp Quest API
-var auth_key = '';
+var auth_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiMjgxMzgyMSIsImV4cCI6OTIyMzM3MjAzNjg1NDc3NSwiYXBwX2lkIjoiNDMzY2JkMTMtMTNmNC00ZWFlLTg1ZmUtN2RkOGNlMmJkNGVhIn0.AeY8PVB5r3pKBPf52APbmQWWweg0vY_78wBkoZNmkmE';
 const PORT = 3000; // The port the server will listen on
 //
 
